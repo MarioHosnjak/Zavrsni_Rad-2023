b59c17c883e375adc13a908249d0e8724e06d04c
Mario Hosnjak <mario.hosnjak009@gmail.com>
2023-05-13 19:57:31 +0200

    Added max-temp reading for each object

--- finalScript.py
+++ finalScript.py
@@ -95,31 +95,30 @@ contours,hierarchy = cv2.findContours(thr, 1, 2)
 
 n = 0
 for cnt in contours:
-   x1,y1 = cnt[0][0]
-   approx = cv2.approxPolyDP(cnt, 0.05*cv2.arcLength(cnt, True), True)
-   if len(approx) == 4:
-      x, y, w, h = cv2.boundingRect(cnt)
-      if w >= 4: # and w < img.shape[1] and h < img.shape[0]:
-        ratio = float(w)/h
-        n = n+1
-        if ratio >= 0.85 and ratio <= 1.15:
-            img = cv2.drawContours(img, [cnt], -1, (0,255,255), 3)
-            cv2.putText(img, f'Square {n}', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
-        else:
-            cv2.putText(img, f'Rectangle {n}', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
-            img = cv2.drawContours(img, [cnt], -1, (255,255,0), 3)
-        # READ TEMPERATURE
-        ##print(f"Object: {n}")
-        ##xt, yt, wt, ht = cv2.boundingRect(cnt)
-        ##maxTemp = [0, 0, 0]
-        ##for i in range(xt, xt + wt):
-        ##    for j in range(yt, yt + ht):
-        ##        temperature_pointer = gray16_frame[j, i]
-        ##        temperature_pointer = (temperature_pointer / 100)
-        ##        if temperature_pointer > maxTemp[0]:
-        ##            maxTemp = [temperature_pointer, i, j]
-        ##print(f"Max temp for object {n} is {maxTemp[0]}°C x={maxTemp[1]} y={maxTemp[2]}")
-        ##cv2.circle(img, (maxTemp[1], maxTemp[2]), 5, (255, 255, 255), -1)
+	x1,y1 = cnt[0][0]
+	approx = cv2.approxPolyDP(cnt, 0.05*cv2.arcLength(cnt, True), True)
+	if len(approx) == 4:
+		x, y, w, h = cv2.boundingRect(cnt)
+		if w >= 20: # and w < img.shape[1] and h < img.shape[0]:
+			ratio = float(w)/h
+			n = n+1
+			if ratio >= 0.85 and ratio <= 1.15:
+				img = cv2.drawContours(img, [cnt], -1, (0,255,255), 3)
+				cv2.putText(img, f'Square {n}', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+			else:
+				cv2.putText(img, f'Rectangle {n}', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+				img = cv2.drawContours(img, [cnt], -1, (255,255,0), 3)
+			# READ TEMPERATURE
+			print(f"Object: {n}")
+			xt, yt, wt, ht = cv2.boundingRect(cnt)
+			maxTemp = [0, 0, 0]
+			for i in range(xt, xt + wt):
+				for j in range(yt, yt + ht):
+					temperature_pointer = frame[int(j / 10) * 32 + int(i / 10)]
+					if temperature_pointer > maxTemp[0]:
+						maxTemp = [temperature_pointer, i, j]
+			print(f"Max temp for object {n} is {maxTemp[0]}°C x={maxTemp[1]} y={maxTemp[2]}")
+			cv2.circle(img, (maxTemp[1], maxTemp[2]), 5, (0, 0, 0), -1)
 
 cv2.imshow("Shapes", img)
 cv2.waitKey(0)
